8dc23948dd979b2ab1f7d81e1fdbe726c95830b3
LeandroCostaB <leandro.boscariol@alunos.bpkedu.com.br>
2024-10-29 22:08:46 -0300

    Cardapio feito

--- schemaTypes/cardapio.js
+++ schemaTypes/cardapio.js
@@ -12,6 +12,22 @@ export const cardapio = defineType({
       title: 'Dia',
     }),
     defineField({
+      name: 'order',
+      type: 'number',
+      title: 'Ordem',
+      options: {
+        list: [
+          { title: '1', value: 1 },
+          { title: '2', value: 2 },
+          { title: '3', value: 3 },
+          { title: '4', value: 4 },
+          { title: '5', value: 5 },
+          { title: '6', value: 6 },
+          { title: '7', value: 7 },
+        ],
+      },
+    }),
+    defineField({
       name: 'principais',
       title: 'Pratos principais',
       type: 'array',
